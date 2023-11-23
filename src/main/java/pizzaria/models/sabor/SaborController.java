6ed65c654658174773ff0ab575f4406fb7da78de
Christian-Eurinidio <113392481+Christian-Eurinidio@users.noreply.github.com>
2023-11-23 17:31:20 -0300

    sabores

--- src/main/java/pizzaria/models/sabor/SaborController.java
+++ src/main/java/pizzaria/models/sabor/SaborController.java
@@ -4,10 +4,18 @@
  */
 package pizzaria.models.sabor;
 
+import pizzaria.models.pedido.view.PedidoFormView;
+
 /**
  *
  * @author chris
  */
 public class SaborController {
+    private PedidoFormView pedidoFormView;
+    private SaborDao saborDao;
     
+    public void initController(PedidoFormView pedidoFormView, SaborDao saborDao) {
+        this.pedidoFormView = pedidoFormView;
+        this.saborDao = saborDao;
+    }
 }
